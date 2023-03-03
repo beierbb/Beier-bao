186606c9ce5f77b7d0c90dc122e3e2bc4efc169a
beierbb <beierbbb@gmail.com>
2023-03-03 11:02:05 -0800

    Mar 3

--- module1/testing/com/shape/moduleFour/TwoDShapeTest.java
+++ module1/testing/com/shape/moduleFour/TwoDShapeTest.java
@@ -1,17 +1,16 @@
 package com.shape.moduleFour;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class TwoDShapeTest {
     @Test public void testGetArea(){
 //  circle
         Circle testCircle = new Circle(3);
         double a = Math.PI;
-        assertEquals(3*3*a, testCircle.getArea(),.0001);
+        Assertions.assertEquals(3*3*a, testCircle.getArea(), .0001);
 //  Triangle
         Triangle testTri = new Triangle(5, 12, 13);
-        assertEquals(30, testTri.getArea(), .0001);
+        Assertions.assertEquals(30, testTri.getArea(), .0001);
     }
 }
